520891b5611395a09e6a72cfffa2d1cf53d401da
David5627 <binginaya708@gmail.com>
2020-11-07 17:21:56 +0300

    delete some quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,13 +9,13 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'JOSEE', "There is only one way to happiness and that is to cease worrying about things beyond your will.",new Date(2020,6,11)),
-    new Quote(2,'BENARD AKAKA',"What you do not want done to yourelf,do not do to others since life is a life cycle and what goes around comes around",new Date(2020,4,12)),
     new Quote(3,'ABDUL khazam.',"Happiness is not something ready made,you must work to find true happiness.",new Date(2020,11,12)),
     new Quote(4,'Mr Psalmist',"Make your life a masterpiece;imagine no limitations on what you can be,have to do.",new Date(2020,11,6)),
     new Quote(5,'BISMILLAHI Mstaf',"You see, the way you think and feel about yourself,including your beliefs and expectations about what is possible for you,determines everything that happens.",new Date(2020,11,6)),
     new Quote(6,'OWEN Gabriel',"The only joy in the world is to begin since a journey of a thousand miles starts with a single step.",new Date(2020,11,6)), 
-    new Quote(6,'David K',"Life is what happens when you're busy making other plans.",new Date(2020,11,7)), 
-    new Quote(6,'David K',"You only live once, but if you do it right, once is enough.",new Date(2020,11,7)), 
+    new Quote(6,'David K',"You only live once, but if you do it right, once is enough.",new Date(2020,11,7)),
+    new Quote(6,'David K',"We all make mistakes, have struggles, and even regret things in our past. But you are not your mistakes, you are not your struggles, and you are here now with the power to shape your day and your future.",new Date(2020,11,7)), 
+ 
 
   ];
 
