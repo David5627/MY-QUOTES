a6bfc609f8913bc52b8994d6e0f8d7f9f32fed52
David5627 <binginaya708@gmail.com>
2020-11-08 15:25:36 +0300

    modify quote component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -4,7 +4,7 @@ import { WeekDay } from '@angular/common';
 
 @Component({
   selector: 'app-quote',
-  templateUrl: './quote.component.html',
+  templateUrl:'./quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
